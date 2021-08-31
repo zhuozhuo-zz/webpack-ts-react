4de7b7543b05b48853ea3da6cf81f1c78de9ab3a
郝卓玉 <haozhuoyu@kaikeba.com>
2021-08-31 13:09:48 +0800

    feat: husky

---
+++ .husky/.commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'build',
+          'ci',
+          'chore',
+          'docs',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'revert',
+          'style',
+          'test'
+        ]
+      ]
+      // 'subject-full-stop': [0, 'never'],
+      // 'subject-case': [0, 'never']
+    }
+  }
